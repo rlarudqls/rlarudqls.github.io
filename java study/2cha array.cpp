1908093716f16348c733c714e9fd254f0d28cdb7
rlarudqls <80000208+rlarudqls@users.noreply.github.com>
2023-03-21 11:31:18 +0900

    Create 2cha array.cpp

---
+++ java study/2cha array.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	int i, n = 0, *ptr;
+	int sale[2][4] = { { 63,57,140,130 }, {157,209,251,312 } };
+
+	ptr = &sale[0][0];
+
+	for (i = 0; i < 8; i++) {
+		printf("\n address : %u sale%d = %d", ptr, i, *ptr);
+	}
+	
+	getchar(); return 0;
+}
