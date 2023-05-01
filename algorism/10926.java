11abfdc1bb2e4f0717e4a6121f0ce15b826e4744
rlarudqls <80000208+rlarudqls@users.noreply.github.com>
2023-05-01 17:16:18 +0900

    Create 10926.java

---
+++ algorism/10926.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        String a = "??!";
+        
+        String b = in.next();
+        
+        System.out.println(b+a);
+    }
+}
