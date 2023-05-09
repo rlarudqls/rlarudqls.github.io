914498a9f2c88a972d9ea645caa04d39c7bcb2d7
rlarudqls <80000208+rlarudqls@users.noreply.github.com>
2023-05-09 11:02:56 +0900

    Create 1330.java

---
+++ algorism/1330.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+
+    Scanner in = new Scanner(System.in);
+    
+    int A = in.nextInt();
+        int B = in.nextInt();
+        
+        in.close();
+        
+        {
+            if(A>B){
+            System.out.println(">");
+        }
+            if(A<B){
+            System.out.println("<");
+        }
+            if(A==B){
+                System.out.println("==");
+            }
+        }
+    }
+}
