13842b98ad67c6f8c753c8b0649fd7fde7d846d7
rlarudqls <80000208+rlarudqls@users.noreply.github.com>
2023-05-10 00:52:39 +0900

    Create 9498.java

---
+++ algorism/9498.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        in.close();
+        
+        if(a>=90) System.out.println("A");
+       else if(a>=80) System.out.println("B");
+       else if(a>=70) System.out.println("C");
+        else if(a>=60) System.out.println("D");
+        else System.out.println("F");
+    } 
+ }
