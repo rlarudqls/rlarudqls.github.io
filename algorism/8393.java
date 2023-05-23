b281236ceb5f5203017ef750a73530f04adbd1b2
rlarudqls <80000208+rlarudqls@users.noreply.github.com>
2023-05-23 18:03:27 +0900

    Create 8393.java

---
+++ algorism/8393.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        
+        Scanner in = new Scanner(System.in);
+        
+        int n = in.nextInt();
+        in.close();
+        int sum = 0;
+        
+        for(int i = 0; i<=n; i++){
+            sum+=i;
+        }
+        
+            System.out.println(sum);
+    }
+}
