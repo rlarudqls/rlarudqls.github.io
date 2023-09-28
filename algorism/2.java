2a85cc76041a750b9d9f90ea2b3306f0a5c54f0b
김경빈 <80000208+rlarudqls@users.noreply.github.com>
2023-09-28 14:22:54 +0900

    Create 2.java

---
+++ algorism/2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int[] arr) {
+        ArrayList<Integer> resultList = new ArrayList<>();
+        resultList.add(arr[0]);
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[i - 1]) {
+                resultList.add(arr[i]);
+            }
+        }
+
+        int[] answer = new int[resultList.size()];
+        for (int i = 0; i < resultList.size(); i++) {
+            answer[i] = resultList.get(i);
+        }
+
+        return answer;
+    }
+}
