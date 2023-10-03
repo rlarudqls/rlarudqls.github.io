8e4a301a524f8f070f7417af776dabd03e317e72
김경빈 <80000208+rlarudqls@users.noreply.github.com>
2023-10-03 14:26:36 +0900

    Create 5.java

---
+++ algorism/5.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        while (n > 0) {
+            answer += n % 10;
+            n /= 10; 
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution(123)); 
+        System.out.println(solution.solution(987));
+    }
+}
